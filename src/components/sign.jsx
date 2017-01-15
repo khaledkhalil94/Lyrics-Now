80aa63204e925186866683cf3e11b063aa45c2b9
khaledkhalil94 <k.khalil.94@gmail.com>
2017-01-15 06:21:40 +0200

    Removed link

--- src/components/sign.jsx
+++ src/components/sign.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import { Icon } from 'semantic-ui-react'
 
-export default () => <a href='http://khaledkhalil.me' id='body-sign'>Made with <Icon color='blue' name='music' /></a>
+export default () => <span id='body-sign'>Made with <Icon color='blue' name='music' /></span>
