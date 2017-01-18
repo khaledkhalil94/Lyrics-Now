c2b022644dbdbc50cb2bd059aef2cfbddac7cddf
khaledkhalil94 <k.khalil.94@gmail.com>
2017-01-18 05:44:34 +0200

    Removed the white overlay when lyrics are not scrollable

--- src/components/lyricsBody/lyricsbody.jsx
+++ src/components/lyricsBody/lyricsbody.jsx
@@ -6,15 +6,19 @@ import {DEF_TRACK_PIC} from '../../constants'
 import Halogen from 'halogen'
 
 class LyricsBody extends Component {
-  componentDidMount() {
+  componentDidUpdate() {
     const element = document.querySelector('.lyrics-body .lyrics')
     const overlayElm = document.querySelector('.lyrics-overlay')
-    element.onscroll = () => {
-      if(50 + element.offsetHeight + element.scrollTop >= element.scrollHeight) {
-        let height = (element.scrollHeight - element.scrollTop - element.offsetHeight) * 2
-        overlayElm.style.height = `${height}px`
-      } else {
-        overlayElm.style.height = '100px'
+    if(element.offsetHeight == element.scrollHeight) overlayElm.style.display = 'none'
+    else {
+      overlayElm.style.display = 'block'
+      element.onscroll = () => {
+        if(50 + element.offsetHeight + element.scrollTop >= element.scrollHeight) {
+          let height = (element.scrollHeight - element.scrollTop - element.offsetHeight) * 2
+          overlayElm.style.height = `${height}px`
+        } else {
+          overlayElm.style.height = '100px'
+        }
       }
     }
   }
