8cc6c61e61bdd30a0e1065d05b3734218035ee5c
khaledkhalil94 <k.khalil.94@gmail.com>
2017-01-29 19:10:06 +0200

    Fixed menu items

--- src/components/header/HeadBarLogged.jsx
+++ src/components/header/HeadBarLogged.jsx
@@ -11,10 +11,10 @@ const Bar = ({user, removeUser, refresh, isHidden, showMenu, hideMenu}) =>
     <Menu.Item header as='a' href={user.url} name={user.realname ? user.realname : user.name} />
     {false && <Menu.Item id='spinner' icon={<Icon name='spinner loading' size='large' />} />}
 
-    <Menu.Menu className='asd' position='right'>
-      {isHidden && <Menu.Item className='logout-btn' as='a' icon='unhide' title='Show recent tracks' onClick={() => showMenu()} />}
-      {!isHidden && <Menu.Item className='logout-btn' as='a' icon='hide' title='Hide recent tracks' onClick={() => hideMenu()} />}
-      <Menu.Item className='logout-btn' as='a' icon='refresh' title='Refresh' onClick={()=> refresh(user.name)} />
+    <Menu.Menu position='right'>
+      {isHidden && <Menu.Item className='hide-btn' as='a' icon='unhide' title='Show recent tracks' onClick={() => showMenu()} />}
+      {!isHidden && <Menu.Item className='hide-btn' as='a' icon='hide' title='Hide recent tracks' onClick={() => hideMenu()} />}
+      <Menu.Item className='refresh-btn' as='a' icon='refresh' title='Refresh' onClick={()=> refresh(user.name)} />
       <Menu.Item className='logout-btn' as='a' icon='log out' title='Logout' onClick={()=> removeUser()} />
     </Menu.Menu>
   </Menu>
